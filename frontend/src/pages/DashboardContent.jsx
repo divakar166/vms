505e8353cd025860e63848fe6caba72efdaf6a41
divakar166 <ds299555@gmail.com>
2024-01-31 13:20:48 +0530

    update

--- frontend/src/pages/DashboardContent.jsx
+++ frontend/src/pages/DashboardContent.jsx
@@ -1,30 +1,37 @@
-import React from 'react'
+import React from 'react';
+import growth from '../images/growth.svg';
 
 const DashboardContent = () => {
   return (
     <div className='m-1'>
-      <div className='bg-stone-600 pl-5 rounded-lg h-40 flex justify-center flex-col'>
-        <div className='text-4xl'>You are doing great </div>
-        <div className='text-3xl font-bold'>Divakar!</div>
-        <div className='text-lg'>Keep it up </div>
+      <div className='bg-blue-100 pl-5 rounded-lg h-40 flex justify-between'>
+        <div className='mt-6'>
+          <div className='text-4xl'>You are doing great </div>
+          <div className='text-3xl font-bold text-blue-500'>Divakar!</div>
+          <div className='text-lg'>Keep it up </div>
+        </div>
+        <div><img src={growth} className='h-[150px] mr-10' alt="" /></div>
       </div>
-      <div className='bg-slate-200 w-[450px] mt-1 pl-5 rounded-lg flex justify-center flex-col'>
+      <div className='bg-slate-200 w-[450px] mt-1 px-5 py-1 rounded-lg flex justify-center flex-col'>
         <div className='flex justify-between'>
-          <div className='flex flex-col p-1'>
-            <div className='font-bold text-3xl'>12</div>
-            <div className='text-sm'>Total Orders</div>
-          </div><hr />
-          <div className='flex flex-col p-1'>
-            <div className='font-bold text-3xl'>8</div>
-            <div className='text-sm'>In Progress</div>
-          </div><hr />
-          <div className='flex flex-col p-1'>
-            <div className='font-bold text-3xl'>3</div>
-            <div className='text-sm'>Completed</div>
-          </div><hr />
-          <div className='flex flex-col p-1'>
-            <div className='font-bold text-3xl'>1</div>
-            <div className='text-sm'>Cancelled</div>
+          <div className='order-item'>
+            <div className='item'>12</div>
+            <div className='item-sub text-slate-600'>Total Orders</div>
+          </div> 
+          <span className='h-14 mt-[10px] border-l-2 border-slate-300' />
+          <div className='order-item'>
+            <div className='item'>8</div>
+            <div className='item-sub text-slate-600'>In Progress</div>
+          </div> 
+          <span className='h-14 mt-[10px] border-l-2 border-slate-300' />
+          <div className='order-item'>
+            <div className='item'>3</div>
+            <div className='item-sub text-slate-600'>Completed</div>
+          </div>
+          <span className='h-14 mt-[10px] border-l-2 border-slate-300' />
+          <div className='order-item'>
+            <div className='item'>1</div>
+            <div className='item-sub text-slate-600'>Cancelled</div>
           </div>
         </div>
       </div>
